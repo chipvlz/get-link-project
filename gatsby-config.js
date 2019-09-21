8759b81cb54f6e20d97e615103286e0a8f21a826
kencoder97 <vuhuucuong97@gmail.com>
2019-09-21 22:31:07 +0700

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Get Link Project',
+        short_name: 'getlink',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.jpg', // This path is relative to the root of the site.
       },
     },
     {
